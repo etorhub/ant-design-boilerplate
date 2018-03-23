a3129b2bebe4b935e0146cc5cfcd5597e7379791
Etor <ediaz@redpoints.com>
2018-03-23 14:19:57 +0100

    fix heroku postbuild

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -27,9 +27,6 @@ const config = {
       minimize: true,
       debug: false,
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-    }),
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
     new ExtractTextPlugin({ filename: './styles/style.css', disable: false, allChunks: true }),
     new CopyWebpackPlugin([{ from: './vendors', to: 'vendors' }]),
