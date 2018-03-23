136f80f11e52f0c448189dfd1d7e7569ed3dda56
Etor <ediaz@redpoints.com>
2018-03-23 14:41:09 +0100

    fix heroku postbuild

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 
 const app = express();
-app.use(express.static(`${__dirname}/`));
+app.use(express.static(`${__dirname}/dist`));
 app.listen(process.env.PORT || 8080);
