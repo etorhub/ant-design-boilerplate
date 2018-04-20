99bbc431263fe4e344742530a1310ef399e342f3
etor <ediaz@netquest.com>
2018-04-20 11:46:22 +0200

    fixed babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,17 @@ const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 const config = {
   devtool: 'cheap-module-eval-source-map',
-
   entry: [
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
     './main.js',
   ],
-
   output: {
     filename: 'bundle.js',
     path: resolve(__dirname, 'dist'),
     publicPath: '',
   },
-
   context: resolve(__dirname, 'app'),
 
   devServer: {
